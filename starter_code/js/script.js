7256e7325367380eb76e8b42e5ab84d3ed10880e
kaziwasitullah <kaziwasitullah@mybihs.org>
2016-01-21 22:58:12 +0000

    flicker search

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,26 @@
 // YOUR CODE GOES HERE
+function callback () {}
+
+$(document).ready(function() {
+    $("#search-btn").click(function (){
+        var name = $("#search-query"). val()
+        console.log (name)
+        searchFlickr(name,callback);
+    }) 
+
+    $("#reset").click(function () {
+       $("#search-query"). val('')
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})
